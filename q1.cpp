8176da5ee5cbf5a6ad24c56b5766f1d30395d48e
ANAGHA-R-NAIR <31248818+ANAGHA-R-NAIR@users.noreply.github.com>
2017-08-22 21:46:51 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+main(){
+char g[50], h[70];
+float a, b, c, d, e, f;
+cout<<"Hi! Enter your name (First Name):";
+cin>>g;
+cout<<"(Second Name):";
+cin>>h;
+cout<<"Enter age=";
+cin>>f;
+cout<<"Your name is "<<g;
+cout<<" "<<h;
+cout<<".";
+cout<<"Your age is "<<f;
+cout<<".";
+cout<<"Enter a number:";
+cin>>a;
+cout<<"Enter another number:";
+cin>>b;
+c=a+b;
+cout<<"Sum="<<c;
+d=a-b;
+cout<<"Difference="<<d;
+e=a*b;
+cout<<"Product="<<e;
+}
+
+
